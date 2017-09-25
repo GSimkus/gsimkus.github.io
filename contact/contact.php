ae8727fffbb593172a1894619323115bb43467f2
Glen Simkus <glen.simkus@gmail.com>
2017-09-25 13:42:41 -0700

    Added pretty much everything

--- contact/contact.php
+++ contact/contact.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Glen Simkus' websiteapalooza</title>
+  <title>Patrick Salembier's contact</title>
   <meta charset="utf-8"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" type="text/css" href="contact.css">
 </head>
 <body>
 	<header class="container">
   	<div class="row">
-      <h1 class="col-sm-4"><a href="index.html">G.S.</a></h1>
+      <h1 class="col-sm-4"><a href="../index.html">P.S.</a></h1>
       <nav class="col-sm-8 text-right">
-        <p><a href="projects/projects.html">Projects</a></p>
-        <p><a href="SimkusResume2017.pdf">Resume PDF Download </a></p>
-        <p><a href="contact/contact.html">Contact</a></p>
+        <p><a href="../projects/projects.html">Projects</a></p>
+        <p><a href="../webMaps/webMaps.html">Web Maps</a></p>
+        <p><a href="../TechResume2016.pdf">Resume PDF Download </a></p>
+        <p><a href="contact.php">Contact</a></p>
       </nav>
     </div>
   </header>
   <section class="container">
     <div class="row">
-      <figure class="col-sm-8">
-        <h3>Bio</h3>
-        <p>Glen has a degree in Chemistry and Microbiology from the University of Victoria, Canada,
-          he codes in Haskell and Python, and freelances scientific and technical writing on the side.
-          Apart from life sciences and bioinformatics he enjoys winter sports - especially skiiing,
-          tabletop boardgames, camping, reading, and grooming his facial hair.</p>
+      <figure class="col-sm-12">
+        <p>I can be reached at patsalembier@gmail.com</p>
       </figure>
-      <figure class="col-sm-4">
-        <img src="selfie.jpg" max-width: 50% class="img-rounded" alt="Responsive image">
+      <figure class="col-sm-5">
+        <p>Don't hesitate to contact me, I'm always up to chat!</p>
       </figure>
     </div>
   </section>
   <footer class="container">
     <div class="row">
-      <p class="col-sm-4">&copy; 2017 Glen Simkus</p>
+      <p class="col-sm-4">&copy; 2016 Patrick Salembier</p>
       <ul class="col-sm-8">
         <li class="col-sm-1">
-        	  <a href="https://github.com/gsimkus">
+        	  <a href="https://github.com/psal93">
               <img src="gitHubLogo_Big.png">
             </a>
         </li>
