4b773b8175da6ccb823275dfc27e83dd0024eb82
Glen Simkus <glen.simkus@gmail.com>
2018-01-12 14:16:57 -0800

    Fixed bug in event listening for projects buttons

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ window.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('resume').addEventListener("click", function(){ ga('send', 'event', 'My Site', 'clickedResume')});
     document.getElementById('transcript').addEventListener("click", function(){ ga('send', 'event', 'My Site', 'clickedTranscript')});
 
-    var classname = document.getElementsByClassName("classname")
+    var classname = document.getElementsByClassName("project")
     for (var i = 0; i < classname.length; i++) {
-        classname[i].addEventListener("click", function(){ ga('send', 'event', 'My Site', 'clickedProject')});
+        classname[i].addEventListener("click", function(){
+          ga('send', 'event', 'My Site', 'clickedProject');
+        });
+
         };
-    console.log("Loaded!");
 });
